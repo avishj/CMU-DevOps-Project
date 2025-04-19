561aa7dd08102d66daddc315d32866ac28af99eb
Avish Jha <avish.j@protonmail.com>
2025-04-19 14:06:44 -0400

    fix: permissions hell
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- scripts/001_file_structure.sh
+++ scripts/001_file_structure.sh
@@ -21,7 +21,7 @@ mkdir -p ../data/zap         # ZAP session data, reports, configs
 touch ../data/prometheus/config/prometheus.yml
 
 # Fix permissions (insecure but simple for local development)
-chmod -R 700 ../data/
+chmod -R 777 ../data/
 
 # Final directory structure:
 
