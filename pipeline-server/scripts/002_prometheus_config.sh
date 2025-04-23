9a2e93206a36f48e2652ace8977a02e6e34ae93b
Avish Jha <avish.j@protonmail.com>
2025-04-23 18:41:04 -0400

    fix: path and port
    Signed-off-by: Avish Jha <avish.j@protonmail.com>

--- pipeline-server/scripts/002_prometheus_config.sh
+++ pipeline-server/scripts/002_prometheus_config.sh
@@ -14,7 +14,7 @@ scrape_configs:
   - job_name: 'jenkins'
     metrics_path: '/jenkins/prometheus/' # Requires Prometheus Metrics Plugin in Jenkins
     static_configs:
-      - targets: ['jenkins:8081']
+      - targets: ['jenkins:8080']
 
   # Add other scrape configs as needed (GitLab, SonarQube, Jenkins...)
   # - job_name: 'gitlab'
